58a56d27923ad81c402a62faae2d7703d0af5088
Salty876 <100157151+Salty876@users.noreply.github.com>
2022-10-08 14:35:59 -0600

    First Teleop Prototype Added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/BotOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/BotOpMode.kt
@@ -18,18 +18,29 @@ class BotOpMode: LinearOpMode() {
             telemetry.addData("Status:", "Is Running")
             val left: Double? = bot!!.forward?.plus(bot?.pivot!!)
             val right: Double? = bot!!.forward?.minus(bot?.pivot!!)
-            val leftPower = right?.let { Range.clip(it, -1.0, 1.0) }
-            val rightPower = left?.let { Range.clip(it, -1.0, 1.0) }
+            val leftPower = right?.let { Range.clip(it, -0.5, 0.5) }
+            val rightPower = left?.let { Range.clip(it, -0.5, 0.5) }
 
             if (leftPower != null || rightPower != null) {
                 if (leftPower != null) {
+                    telemetry.addData("Left Control","True")
                     bot!!.fl.power = leftPower
                     bot!!.bl.power = leftPower
                 }
                 if (rightPower != null) {
+                    telemetry.addData("Right Control","True")
                     bot!!.fr.power = rightPower
                     bot!!.br.power = rightPower
                 }
+                sleep(250)
+                bot!!.fr.power = 0.0
+                bot!!.br.power = 0.0
+                bot!!.fl.power = 0.0
+                bot!!.bl.power = 0.0
+
+                telemetry.update()
+
+
             }
             telemetry.update()
         }
